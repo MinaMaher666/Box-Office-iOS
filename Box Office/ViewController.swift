e7ad6d48b5a245fdbfcd89150e9ea5b2d64c7aba
Mina Maher <R3V@macs-macbook-pro.local>
2018-03-08 23:02:20 +0200

    Initial Commit

---
+++ Box Office/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Box Office
+//
+//  Created by Mina Maher on 3/8/18.
+//  Copyright © 2018 Mina R3V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
